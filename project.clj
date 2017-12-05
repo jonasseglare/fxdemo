cca7557ba7cce7ccc5ce978f2b64f9d2d7f3358c
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-12-05 22:42:13 +0100

    good

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["jzy3d-releases" "https://maven.jzy3d.org/releases"]]
   :aot [fxdemo.example1
         fxdemo.example2]
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.jzy3d/jzy3d-api "1.0.0"]])
